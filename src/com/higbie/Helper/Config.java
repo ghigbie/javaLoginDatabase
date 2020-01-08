32bad5137fbe883739ab0c6111abd0fcc52086f0
ghigbie <gehigbie@gmail.com>
2020-01-07 20:17:24 -0700

    Add sql command

--- src/com/higbie/Helper/Config.java
+++ src/com/higbie/Helper/Config.java
@@ -1,7 +1,6 @@
 package com.higbie.Helper;
 
 public class Config {
-
     protected String dbHost = "localhost";
     protected String dbPort = "3306";
     protected String dbUser = "root";
--- src/com/higbie/Main.java
+++ src/com/higbie/Main.java
@@ -1,17 +1,28 @@
 package com.higbie;
 
+import com.higbie.Helper.DBHandler;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
+
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class Main {
+    static private DBHandler dbHandler;
+    static private Connection connection;
+    static private PreparedStatement preparedStatement;
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-	// write your code here
-        Class.forName("com.mysql.jdbc.Driver");
-        Connection connection =
-                DriverManager.getConnection("jdbc:mysql://localhost:3306/login",
-                        "root", "secret123");
-        System.out.println("Connected to the database " + connection.getCatalog());
-    }
+        dbHandler = new DBHandler();
+        connection = dbHandler.getDbConnection();
+        String insert = "INSERT INTO users(firstname, lastname, username, address, age )"
+                + "VALUES(?, ?, ?, ?, ?)";
+
+        preparedStatement = connection.prepareStatement(insert);
+        preparedStatement.setString(1, "Paulo");
+        preparedStatement.setString(2, "Dichone");
+        preparedStatement.setString(3, "pdichone");
+        preparedStatement.setString(4, "123 Park Ave");
+        preparedStatement.setInt(5, 33);
 }
